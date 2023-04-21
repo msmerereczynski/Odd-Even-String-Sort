d60104faaf522b8785c63b3cb854f92119e77a12
m.smereczynski <m.smereczynski@H91004336>
2023-04-21 13:11:45 +0200

    Add project files.

---
+++ Odd-Even String Sort/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odd_Even_String_Sort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Char> nieparzyste = new List<char>();
+            List<Char> parzyste = new List<Char>();
+            Console.WriteLine("Give string");
+            string s = Console.ReadLine();
+            char[]s_array = s.ToCharArray();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    parzyste.Add (s_array[i]);
+                }
+                else
+                {
+                    nieparzyste.Add(s_array[i]);
+                }
+            }
+            Console.WriteLine(new string(parzyste.ToArray()) + " " + new string(nieparzyste.ToArray()));
+            Console.ReadKey();
+        }
+    }
+}
